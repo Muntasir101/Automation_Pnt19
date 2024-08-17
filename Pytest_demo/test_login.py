fb5d177582249bb5a3852f8788d3314ef7fdffcd
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-08-17 12:53:22 +0600

    pytest

---
+++ Pytest_demo/test_login.py
@@ -0,0 +1,10 @@
+def test_testCase1():
+    print("Testing Test Case 1 done")
+
+
+def test_testCase2():
+    print("Testing Test Case 2 done")
+
+
+def test_testCase3():
+    print("Testing Test Case 3 done")
